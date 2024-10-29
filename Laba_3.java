5c6b76a016e07bcd9753699f63ef5fdd7fda72c9
Marat Ayupov <maratayup@yandex.ru>
2024-10-29 11:55:15 +0300

    ждем Никиту

--- Laba_3.java
+++ Laba_3.java
@@ -1,12 +1,8 @@
 import UI.UI;
-import realization.Student;
 
 public class Laba_3 {
     public static void main(String[] args) {
-        // UI user = new UI();
-        // user.run();
-        Student.coutListStudents();
-        Student.sortArrayStudents();
-        Student.coutListStudents();
+        UI user = new UI();
+        user.run();
     }
 }
